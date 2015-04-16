4a9c24a9628fc9da59b6749e12bb566efb5e821e
Colin T.A. Gray <colinta@gmail.com>
2015-04-16 14:14:22 -0600

    support for duplicating the last line of a file

--- duplicate_selections.py
+++ duplicate_selections.py
@@ -23,24 +23,43 @@ class DuplicateSelectionsCommand(sublime_plugin.TextCommand):
             # gather all the lines
             lines = []
             for cursor in empties:
-                lines.append(self.view.substr(self.view.full_line(cursor)))
+                line_region = self.view.full_line(cursor)
+                line = self.view.substr(line_region)
+                lines.append(line)
+
             # insert them
             self.view.sel().clear()
             lines.reverse()
             empties.reverse()
+            last_row, _ = self.view.rowcol(len(self.view))
             for region_i, cursor in enumerate(empties):
                 row, col = self.view.rowcol(cursor.a)
-                if col == 0:
+                line = lines[region_i]
+
+                if row == last_row:
+                    # if we're on the last line, things are hairy; we need to
+                    # insert an extra newline somewhere, and account for that by
+                    # moving col around.
+                    if col == 0:
+                        insert_point = cursor.a
+                        line = line + "\n"
+                    else:
+                        insert_point = self.view.full_line(cursor).end()
+                        col += 1
+                        line = "\n" + line
+                elif col == 0:
                     insert_point = cursor.a
                 else:
                     insert_point = self.view.full_line(cursor).end()
-                self.view.insert(edit, insert_point, lines[region_i])
+                self.view.insert(edit, insert_point, line)
                 insert_point += col
                 self.view.sel().add(sublime.Region(insert_point, insert_point))
             return
+
         if not empties:
             sublime.status_message("No empty regions")
             return
+
         if len(notempties) > 1 and len(empties) != len(notempties):
             sublime.status_message("Number of empty regions (%i) does note equal number of non-empty regions (%i)" % (len(empties), len(notempties)))
             return
